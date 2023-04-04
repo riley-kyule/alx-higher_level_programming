b337fa7fa29c328dee10f7a9b686bd9c8cbace6d
root <phillipkyule@gmail.com>
2023-04-04 12:37:34 -0700

    Removed whitespace

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,10 +2,9 @@
 """A script that:
 - takes in a URL,
 - sends a request to the URL
-- displays the body of the response 
+- displays the body of the response
 - (decoded in utf-8).
 """
-
 
 if __name__ == "__main__":
     import sys
