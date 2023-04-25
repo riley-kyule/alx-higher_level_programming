239117e191b254b36e851d42f972e1b56ce9ded3
riley-kyule <phillipkyule@gmail.com>
2023-04-26 00:28:43 +0300

    Task 7. Who was playing in this movie? - Prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,47 +1,20 @@
 #!/usr/bin/node
-const base64 = require('base-64');
-const request = require('request');
-const utf8 = require('utf8');
-
-let promise = new Promise(function (resolve, reject) {
-  const token = utf8.decode(base64.encode(`${process.argv[2]}:${process.argv[3]}`));
-  const options = {
-    url: 'https://api.twitter.com/oauth2/token',
-    headers: {
-      Authorization: `Basic ${token}`,
-      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-    },
-    form: {
-      grant_type: 'client_credentials'
-    }
-  };
-  request.post(options, function (error, response, body) {
-    if (!error) {
-      resolve(JSON.parse(body).access_token);
-    }
-  });
+const req = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/';
+req.get(url + id, function (error, res, body) {
+  if (error) {
+    console.log(error);
+  }
+  const data = JSON.parse(body);
+  const dd = data.characters;
+  for (const i of dd) {
+    req.get(i, function (error, res, body1) {
+      if (error) {
+        console.log(error);
+      }
+      const data1 = JSON.parse(body1);
+      console.log(data1.name);
+    });
+  }
 });
-
-promise.then(
-  result => search(result),
-  error => console.log(error)
-);
-
-function search (bearer) {
-  const options = {
-    url: 'https://api.twitter.com/1.1/search/tweets.json',
-    headers: {
-      Authorization: `Bearer ${bearer}`
-    },
-    qs: {
-      q: process.argv[4],
-      count: '5'
-    }
-  };
-  request.get(options, function (error, response, body) {
-    if (!error) {
-      const tweets = JSON.parse(body).statuses;
-      tweets.forEach((t) => console.log(`[${t.id}] ${t.text} by ${t.user.name}`));
-    }
-  });
-}
