0a5073e6d23ae841d5e22acf6dad4546290d01b6
riley-kyule <phillipkyule@gmail.com>
2023-04-25 16:52:32 +0300

    Task 0. Readme - Script that reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', function (err, content) {
-  console.log(err || content);
+const file = process.argv[2];
+fs.readFile(file, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
 
